19ddba68a8aaaf7031a27c4f776ef9e3a3f700b1
azita <azitaabdollahi75@gmial.com>
2023-09-01 02:02:10 +0330

    feat: add book model

---
+++ sc-pr/src/models/book.model.js
@@ -0,0 +1,36 @@
+const models = require('../utils/scyllaDBConnect/index');
+
+var bookModel = models.loadSchema('books', {
+    fields:{
+        id: {
+            type: "uuid",
+            default: {"$db_function": "uuid()"},
+        },
+        author: {
+            type: "int",
+        },
+        title: {
+            type: "text",
+        },
+        description: {
+            type: "text"
+        },
+        publishDate: {
+            type: "timestamp"
+        },
+        created: {
+            type: "timestamp",
+            default: {"$db_function": "toTimestamp(now())"}
+        },
+        edition: {
+            type: "int"
+        }
+    },  
+    key:["id"],
+});
+
+console.log(models.instance.books === bookModel);
+bookModel.syncDB(function(err, result) {
+    if (err) throw err;
+});
+module.exports = bookModel;
